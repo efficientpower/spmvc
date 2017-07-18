0048298dd6938765e95f9da61591b7b1f58870bb
wangjihui <wangjihui@debian>
2017-07-19 00:27:18 +0800

    mod:gouzao

--- src/main/java/org/wjh/common/HelloNotifier.java
+++ src/main/java/org/wjh/common/HelloNotifier.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HelloNotifier {
+    
+    public HelloNotifier() {
+        System.out.println("HelloNotifier 构造方法");
+        // TODO Auto-generated constructor stub
+    }
+
     public void send(){
         System.out.println("HelloNotifier");
     }
