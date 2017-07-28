c74ef2e95a4ce6d746988fc12b326534812c698c
wangjihui <wangjihui@BIH-D-5596.netease.internal>
2017-07-28 18:29:00 +0800

    mod:car

--- src/main/java/org/wjh/domain/Car.java
+++ src/main/java/org/wjh/domain/Car.java
@@ -3,6 +3,11 @@ package org.wjh.domain;
 public class Car {
     public String name;
 
+    public Car() {
+        // TODO Auto-generated constructor stub
+        System.out.println("Car 构造方法");
+    }
+
     public String getName() {
         return name;
     }
