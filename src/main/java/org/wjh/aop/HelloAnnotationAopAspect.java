0ab4290aaa1edbb9d1bc01e81a5703404e8e2274
wangjihui <wangjihui@BIH-D-5596.netease.internal>
2017-11-02 17:31:39 +0800

    mod:注解通知新增

--- src/main/java/org/wjh/aop/HelloAnnotationAopAspect.java
+++ src/main/java/org/wjh/aop/HelloAnnotationAopAspect.java
@@ -1,7 +1,10 @@
 package org.wjh.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -17,11 +20,27 @@ public class HelloAnnotationAopAspect {
     
     @Before("HelloAnnotationAopAspectPointcut()")
     public void before(JoinPoint joinPoint){
-        System.out.println("HelloAnnotationAopAspect @Before method=" + joinPoint.getTarget());
+        System.out.println("HelloAnnotationAopAspect @Before method=" + joinPoint.getSignature().getName());
     }
 
     @After("HelloAnnotationAopAspectPointcut()")
     public void after(JoinPoint joinPoint){
-        System.out.println("HelloAnnotationAopAspect @After method=" + joinPoint.getTarget());
+        System.out.println("HelloAnnotationAopAspect @After method=" + joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning("HelloAnnotationAopAspectPointcut()")
+    public void afterReturning(JoinPoint joinPoint){
+        System.out.println("HelloAnnotationAopAspect @AfterReturning method=" + joinPoint.getSignature().getName());
+    }
+
+    @Around("HelloAnnotationAopAspectPointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable{
+        /**
+         * 环绕通知连接点参数必须是ProceedingJoinPoint类型,他是JoinPoint的子类,需要明确调用ProceedingJoinPoint.proceed()方法
+         * 如果忘记会导致通知被执行,但是目标方法没有执行。
+         * 环绕通知需要返回目标方法执行后的结果,否则可能会导致空指针异常
+         */
+        System.out.println("HelloAnnotationAopAspect @Around method=" + joinPoint.getSignature().getName());
+        return joinPoint.proceed();
     }
 }
