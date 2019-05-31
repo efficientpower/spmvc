a781b1b27992b65ecac616d2e45ac99196dc8c38
wangjihui <wangjihuiupc@163.com>
2019-05-31 09:35:54 +0800

    code review

--- src/main/java/org/wjh/annotation/User.java
+++ src/main/java/org/wjh/annotation/User.java
@@ -1,13 +1,11 @@
 package org.wjh.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * 判定是否需要登录
  */
+@Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface User {
