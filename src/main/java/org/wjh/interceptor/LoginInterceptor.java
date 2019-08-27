a05e6b1d996c636dc205986858172cee32eae7b2
王吉辉 <wangjh@hiysg.com>
2019-08-27 17:43:57 +0800

    code review

--- src/main/java/org/wjh/interceptor/LoginInterceptor.java
+++ src/main/java/org/wjh/interceptor/LoginInterceptor.java
@@ -38,6 +38,6 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
+        logger.info("LoginInterceptor.afterCompletion()");
     }
 }
